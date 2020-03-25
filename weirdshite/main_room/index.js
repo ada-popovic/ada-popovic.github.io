332c23be35b84478c2162335962571c654c48a28
ada popovic <popowucz.adrianna@gmail.com>
2020-03-25 11:11:28 +0100

    weirdshite

---
+++ weirdshite/main_room/index.js
@@ -0,0 +1,10 @@
+$('.sweden').click(function() {
+    $('.meatballs').css({
+        'margin-left': '490px',
+    });
+});
+
+
+$('satan').on('mouseover', function() {
+  $('cosmos').find('img').attr('src', "images/cosmos.gif");
+});
