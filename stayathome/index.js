58626be8a0e332015ba880428918b93407a174a1
ada popovic <popowucz.adrianna@gmail.com>
2020-03-29 12:02:40 +0200

    hello

---
+++ stayathome/index.js
@@ -0,0 +1,100 @@
+$( '.image' ).draggable( {
+      containment: 'window',
+      scroll: false,
+       drag: function( ) {
+         dragging = true;
+       },
+       stop: function( ) {
+         dragging = false;
+       }
+    } );
+
+
+
+
+
+
+$(document).ready(function() {
+  var $art = $(".art");
+  var $blocks = $(".block");
+  var $lis = $(".block li");
+  var $control = $(".control");
+  var $inputs = $(".control input");
+  var $image = $(".image");
+  var $save = $(".trigger-save");
+  var $reset = $(".trigger-reset");
+  var $output = $("output");
+
+  $control.on("keyup", ".text-data", updateText)
+    .on("blur", ".image-data", updateImage);
+
+  $blocks.on("click", function() {
+    highlight($(".text-data"));
+  });
+
+  $image.on("click", function() {
+    highlight($(".image-data"));
+  });
+
+  $save.on("click", saveImage);
+  $reset.on("click", reset);
+
+  function reset() {
+    $inputs.val("");
+    updateText();
+    updateImage();
+  }
+
+  function updateText(){
+    var val = $(this).val();
+    if (val == "") {
+      val = "stay at home!";
+    }
+    val = val.split(" ").join("&nbsp;&nbsp;");
+    $lis.html(val);
+  }
+
+  function updateImage() {
+    var val = $(this).val();
+    if (val == "") {
+      val = "http://crossorigin.me/https://s3-us-west-2.amazonaws.com/s.cdpn.io/42746/lmao.jpg";
+    }
+    getDataUri('http://crossorigin.me/' + val, function(dataUri) {
+      $image.attr("style", "background-image: url(" + dataUri + ");")
+    });
+  }
+
+  function saveImage() {
+    html2canvas($art).then(function(canvas) {
+      $output.append(canvas);
+       var a = document.createElement('a');
+        // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+        a.href = canvas.toDataURL();
+        a.download = 'wash-your-hands.jpg';
+        a.click();
+    });
+  }
+
+  function highlight($el) {
+    $control.addClass("highlight");
+    $el.focus();
+    setTimeout(function() {
+      $control.removeClass("highlight");
+    }, 500)
+  }
+
+  function getDataUri(url, callback) {
+    var image = new Image();
+    image.crossOrigin="anonymous"
+    image.onload = function() {
+      var canvas = document.createElement('canvas');
+      canvas.width = this.naturalWidth;
+      canvas.height = this.naturalHeight;
+      canvas.getContext('2d').drawImage(this, 0, 0);
+      callback(canvas.toDataURL('image/png'));
+    };
+    image.src = url;
+  }
+
+  reset();
+});
