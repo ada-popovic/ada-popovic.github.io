cb65b604ad00383f23b96d8465f2ae2b8dfff467
ada popovic <popowucz.adrianna@gmail.com>
2020-04-20 00:59:57 +0200

    --

---
+++ posterist/script.js
@@ -0,0 +1,84 @@
+var myArray1 = [
+  "things i am good at",
+  "things i like",
+  "tastiness",
+  "self",
+  "tastiness",
+  "homeostasis",
+  "hysteria"
+];
+
+var randomItem1 = myArray1[Math.floor(Math.random()*myArray1.length)];
+
+document.getElementById('word1').innerHTML = randomItem1;
+
+var myArray2 = [
+  "things i am good at",
+  "things i like",
+  "tastiness",
+  "self",
+  "tastiness",
+  "homeostasis",
+  "hysteria"
+];
+
+
+var randomItem2 = myArray2[Math.floor(Math.random()*myArray2.length)];
+
+
+
+
+document.getElementById('wordD2').innerHTML = randomItem2;
+
+
+var myArray3 = [
+  "things i am good at",
+  "things i like",
+  "tastiness",
+  "self",
+  "tastiness",
+  "homeostasis",
+  "hysteria"
+];
+
+
+var randomItem3 = myArray3[Math.floor(Math.random()*myArray3.length)];
+
+document.getElementById('word3').innerHTML = randomItem3;
+
+
+
+
+var myArray4 = [
+  "things i am good at",
+  "things i like",
+  "tastiness",
+  "self",
+  "tastiness",
+  "homeostasis",
+  "hysteria"
+];
+
+
+var randomItem4 = myArray4[Math.floor(Math.random()*myArray4.length)];
+
+document.getElementById('wordD4').innerHTML = randomItem4;
+
+
+
+
+
+var myArray5 = [
+  "things i am good at",
+  "things i like",
+  "tastiness",
+  "self",
+  "tastiness",
+  "homeostasis",
+  "hysteria"
+];
+
+
+var randomItem5 = myArray5[Math.floor(Math.random()*myArray5.length)];
+
+document.getElementById('word5').innerHTML = randomItem5;
